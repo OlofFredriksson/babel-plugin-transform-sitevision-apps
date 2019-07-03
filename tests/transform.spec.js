a76b9a44495501704d43c401c09d10751ce6b421
Olof Fredriksson <fredriksson@olof.nu>
2019-07-03 20:11:53 +0200

    test cleanup

--- tests/transform.spec.js
+++ tests/transform.spec.js
@@ -3,11 +3,16 @@ import path from "path";
 import fs from "fs";
 
 const encoding = "UTF-8";
+const pluginPath = "../src/transform";
 
 const defaultConfig = {
 	babelrc: false,
-	plugins: [path.join(__dirname, "../src/transform")],
+	plugins: [path.join(__dirname, pluginPath)],
 };
+
+function readFile(fileName) {
+	return fs.readFileSync(path.join(__dirname, fileName), encoding);
+}
 
 function compileSource(source, config) {
 	const plugins = Object.assign({}, defaultConfig, config);
@@ -26,43 +31,33 @@ function compileFile(fileName, config) {
 
 describe("compiling source (static exports name)", () => {
 	test("exported function", () => {
-		const source = fs.readFileSync(
-			path.join(__dirname, "fixtures/exported-function.js"),
-			encoding
-		);
-		expect(compileSource(source)).toMatchSnapshot();
+		expect(
+			compileSource(readFile("fixtures/exported-function.js"))
+		).toMatchSnapshot();
 	});
 
 	test("server module", () => {
-		const source = fs.readFileSync(
-			path.join(__dirname, "fixtures/server.module.js"),
-			encoding
-		);
-		expect(compileSource(source)).toMatchSnapshot();
+		expect(
+			compileSource(readFile("fixtures/server.module.js"))
+		).toMatchSnapshot();
 	});
 
 	test("main.js", () => {
-		const source = fs.readFileSync(
-			path.join(__dirname, "fixtures/main.js"),
-			encoding
-		);
 		const config = {
-			plugins: [[path.join(__dirname, "../src/transform"), { type: "mainjs" }]],
+			plugins: [[path.join(__dirname, pluginPath), { type: "mainjs" }]],
 		};
-		expect(compileSource(source, config)).toMatchSnapshot();
+		expect(
+			compileSource(readFile("fixtures/main.js"), config)
+		).toMatchSnapshot();
 	});
 
 	test("index.js", () => {
-		const source = fs.readFileSync(
-			path.join(__dirname, "fixtures/index.js"),
-			encoding
-		);
 		const config = {
-			plugins: [
-				[path.join(__dirname, "../src/transform"), { type: "indexjs" }],
-			],
+			plugins: [[path.join(__dirname, pluginPath), { type: "indexjs" }]],
 		};
-		expect(compileSource(source, config)).toMatchSnapshot();
+		expect(
+			compileSource(readFile("fixtures/index.js"), config)
+		).toMatchSnapshot();
 	});
 });
 
@@ -74,17 +69,11 @@ describe("compiling files (dynamic exports name)", () => {
 
 describe("error handling", () => {
 	test("should fail with invalid SV type", () => {
-		const source = fs.readFileSync(
-			path.join(__dirname, "fixtures/main.js"),
-			encoding
-		);
 		const config = {
-			plugins: [
-				[path.join(__dirname, "../src/transform"), { type: "invalidType" }],
-			],
+			plugins: [[path.join(__dirname, pluginPath), { type: "invalidType" }]],
 		};
 		expect(() => {
-			compileSource(source, config);
+			compileSource(readFile("fixtures/main.js"), config);
 		}).toThrow();
 	});
 });
